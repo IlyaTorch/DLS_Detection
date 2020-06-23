9a4c3efac48e6a72ce2e1608cfee359da3b55dc0
IlyaTorch <itorch2001@gmail.com>
2020-06-23 13:11:54 +0300

    change structure of the app

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from config import Configuration
-from detection.blueprint import detection
+import cv2
+from src.WorkWithImage import WorkWithImage
+
+import torchvision
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 # to disable caching files
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-app.register_blueprint(detection, url_prefix='/detection')
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+model = model.eval()
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def detect_image():
+    img = img_url = None
+    try:
+        img = request.files['image']
+    except KeyError:
+        try:
+            img_url = request.form['image_url']
+        except KeyError:
+            return render_template('error.html')
+    if img:
+        WorkWithImage.save_image_from_bytes(img)
+
+    if img_url:
+        WorkWithImage.save_image_from_url(img_url)
+
+    img, img_tensor = WorkWithImage.read_image_to_numpy_and_tensor('static/cache/image_before.jpg')
+    boxes, labels = WorkWithImage.get_prediction(img_tensor, model)
+    img_with_boxes = WorkWithImage.plot_boxes_and_labels(img, boxes, labels)
+    cv2.imwrite('static/cache/image_after.jpg', img_with_boxes)
+
+    return render_template('detection_result.html')
--- view.py
+++
@@ -1,7 +0,0 @@
-from app import app
-from flask import render_template, redirect
-
-
-@app.route('/')
-def index():
-    return redirect('detection')
